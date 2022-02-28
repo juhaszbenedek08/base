c05370f153c8b27666fcc1f186790162e7311009
Matyi00 <p.matyi00@gmail.com>
2022-02-28 16:48:21 +0100

    added tachograph

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
@@ -0,0 +1,18 @@
+package hu.bme.mit.train.tachograph;
+
+import hu.bme.mit.train.system.TrainSystem;
+import hu.bme.mit.train.user.
+import com.google.common.collect.*;
+
+
+public class Tachograph{
+	private TrainSystem trainSystem = new TrainSystem;
+
+	private Table<Integer, Integer, Integer> records = HashBasedTable.create();
+
+	public void record(int time)
+	{
+		records.put(time, TrainSystem.getUser().getJoystickPosition(), TrainSystem.getController().getReferenceSpeed());
+	}
+
+}
