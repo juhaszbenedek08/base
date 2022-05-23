46b0e4ce0d0eff6d336eb15a1334d7c2392cb3be
juhaszbenedek08 <59647190+juhaszbenedek08@users.noreply.github.com>
2022-05-23 14:39:43 +0200

    Timing function created

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -7,6 +7,9 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainSystem {
 
 	private TrainController controller = new TrainControllerImpl();
@@ -23,6 +26,16 @@ public class TrainSystem {
 
 	public TrainUser getUser() {
 		return user;
+	}
+	
+	public void run(){
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				controller.setJoystickPosition(user.getJoystickPosition());
+			}
+		}, 1000);
 	}
 
 }
